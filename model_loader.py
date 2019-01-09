5771a5580ae3e8cfdec7ddaed631b15b32689a8c
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2019-01-09 11:10:28 +0100

    Refactor model loader

--- model_loader.py
+++ model_loader.py
@@ -1,45 +1,67 @@
 class ModelLoader:
-    def __init__(self, filename, swapyz=False):
-        """Loads a Wavefront OBJ file. """
+
+    def __init__(self, filename, swap_yz=False):
         self.vertices = []
         self.normals = []
-        self.texcoords = []
+        self.texture_coords = []
         self.faces = []
-        material = None
-        for line in open(filename, "r"):
-            if line.startswith('#'): continue
+
+        self.filename = filename
+        self.swap_yz = swap_yz
+
+        self.parse_model_obj()
+
+    def parse_model_obj(self):
+        """Parse model with .OBJ file format."""
+
+        for line in open(self.filename, "r"):
+            if line.startswith('#'):
+                continue
+
             values = line.split()
-            if not values: continue
-            if values[0] == 'v':
+
+            if not values:
+                continue
+
+            line_symbol = values[0]
+
+            # Geometric vertices, with (x, y, z [,w])
+            # Note: w is optional and defaults to 1.0
+            if line_symbol == 'v':
                 v = list(map(float, values[1:4]))
-                if swapyz:
+                if self.swap_yz:
                     v = v[0], v[2], v[1]
                 self.vertices.append(v)
-            elif values[0] == 'vn':
+
+            # Vertex normals in (x,y,z)
+            elif line_symbol == 'vn':
                 v = list(map(float, values[1:4]))
-                if swapyz:
+                if self.swap_yz:
                     v = v[0], v[2], v[1]
                 self.normals.append(v)
-            elif values[0] == 'vt':
-                self.texcoords.append(list(map(float, values[1:3])))
-            #elif values[0] in ('usemtl', 'usemat'):
-                #material = values[1]
-            #elif values[0] == 'mtllib':
-                #self.mtl = MTL(values[1])
-            elif values[0] == 'f':
+
+            # Texture coordinates, in (u, [v ,w])
+            elif line_symbol == 'vt':
+                self.texture_coords.append(list(map(float, values[1:3])))
+
+            # Polygonal face element
+            elif line_symbol == 'f':
                 face = []
-                texcoords = []
+                texture_coords = []
                 norms = []
+
                 for v in values[1:]:
                     w = v.split('/')
                     face.append(int(w[0]))
+
                     if len(w) >= 2 and len(w[1]) > 0:
-                        texcoords.append(int(w[1]))
+                        texture_coords.append(int(w[1]))
                     else:
-                        texcoords.append(0)
+                        texture_coords.append(0)
+
                     if len(w) >= 3 and len(w[2]) > 0:
                         norms.append(int(w[2]))
                     else:
                         norms.append(0)
-                #self.faces.append((face, norms, texcoords, material))
-                self.faces.append((face, norms, texcoords))
+
+                self.faces.append((face, norms, texture_coords))
